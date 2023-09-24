22608e25591755facaa3f981fca0eb5aa0a8368c
YaelZamora <ajedrezkasparov@gmail.com>
2023-09-24 00:02:28 -0600

    Name

--- ProyectOne/ContentView.swift
+++ ProyectOne/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Es mi primer proyecto")
+            Text("Yunhuen")
         }
         .padding()
     }
