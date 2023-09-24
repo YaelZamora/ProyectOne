cd81a4a509f806fc3766a6994cc02b87a866d2b2
YaelZamora <ajedrezkasparov@gmail.com>
2023-09-23 23:37:19 -0600

    Initial Commit

---
+++ ProyectOne/ProyectOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectOneApp.swift
+//  ProyectOne
+//
+//  Created by Yael Javier Zamora Moreno on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
